3c94a3a6c1def53e2850d6e2819f35ca9ab31c06
vuk95 <vuk95novakovic@gmail.com>
2018-07-30 19:57:47 +0200

    Incijalni komit

---
+++ BookingClone/src/main/java/com/xmlws/ws/BookingCloneApplication.java
@@ -0,0 +1,12 @@
+package com.xmlws.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingCloneApplication.class, args);
+	}
+}
