0eea755c8527d56c606309393d641eb5acdc121f
Dimitrije <dimmihzr@gmail.com>
2018-08-19 16:46:04 +0200

    Promenjen xsd, i izmenjen model(Svi korisnici su promenjeni)

--- Backend/src/main/java/com/xmlws/admin/model/Agent.java
+++ Backend/src/main/java/com/xmlws/admin/model/Agent.java
@@ -9,192 +9,36 @@
 package com.xmlws.admin.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for anonymous complex type.
+ * <p>Java class for Agent complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType&gt;
- *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
- *       &lt;sequence&gt;
- *         &lt;element name="Ime" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
- *         &lt;element name="Prezime" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
- *         &lt;element name="Email" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
- *         &lt;element name="Lozinka" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
- *         &lt;element ref="{}Adresa"/&gt;
- *         &lt;element name="Poslovni_maticni_broj" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
- *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}long"/&gt;
- *       &lt;/sequence&gt;
- *     &lt;/restriction&gt;
- *   &lt;/complexContent&gt;
- * &lt;/complexType&gt;
+ * &lt;complexType name="Agent">
+ *   &lt;complexContent>
+ *     &lt;extension base="{}Korisnik">
+ *       &lt;attribute name="Poslovni_maticni_broj" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
  * </pre>
  * 
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "", propOrder = {
-    "ime",
-    "prezime",
-    "email",
-    "lozinka",
-    "adresa",
-    "poslovniMaticniBroj",
-    "id"
-})
+@XmlType(name = "Agent")
 @Entity
-@XmlRootElement(name = "Agent")
-public class Agent {
+public class Agent extends Korisnik{
 
-    @XmlElement(name = "Ime", required = true)
-    protected String ime;
-    @XmlElement(name = "Prezime", required = true)
-    protected String prezime;
-    @XmlElement(name = "Email", required = true)
-    protected String email;
-    @XmlElement(name = "Lozinka", required = true)
-    protected String lozinka;
-    @XmlElement(name = "Adresa", required = true)
-    @ManyToOne
-    protected Adresa adresa;
-    @XmlElement(name = "Poslovni_maticni_broj", required = true)
+    @XmlAttribute(name = "Poslovni_maticni_broj")
     protected String poslovniMaticniBroj;
-    @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long id;
-
-    /**
-     * Gets the value of the ime property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getIme() {
-        return ime;
-    }
-
-    /**
-     * Sets the value of the ime property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setIme(String value) {
-        this.ime = value;
-    }
-
-    /**
-     * Gets the value of the prezime property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getPrezime() {
-        return prezime;
-    }
-
-    /**
-     * Sets the value of the prezime property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setPrezime(String value) {
-        this.prezime = value;
-    }
-
-    /**
-     * Gets the value of the email property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Sets the value of the email property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setEmail(String value) {
-        this.email = value;
-    }
-
-    /**
-     * Gets the value of the lozinka property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getLozinka() {
-        return lozinka;
-    }
-
-    /**
-     * Sets the value of the lozinka property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setLozinka(String value) {
-        this.lozinka = value;
-    }
-
-    /**
-     * Gets the value of the adresa property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link Adresa }
-     *     
-     */
-    public Adresa getAdresa() {
-        return adresa;
-    }
-
-    /**
-     * Sets the value of the adresa property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link Adresa }
-     *     
-     */
-    public void setAdresa(Adresa value) {
-        this.adresa = value;
-    }
 
     /**
      * Gets the value of the poslovniMaticniBroj property.
@@ -220,20 +64,5 @@ public class Agent {
         this.poslovniMaticniBroj = value;
     }
 
-    /**
-     * Gets the value of the id property.
-     * 
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * Sets the value of the id property.
-     * 
-     */
-    public void setId(Long value) {
-        this.id = value;
-    }
-
 }
+
