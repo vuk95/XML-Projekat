4306e639d8c6b0ab55cb778e834fd863755a9825
Milovic <Milovic@DESKTOP-BHBIRQC>
2018-08-21 17:40:11 +0200

    Zapocet detaljniji prikaz rezultata pretrage.

--- BookingClone/src/main/java/com/xmlws/ws/controller/PoundaController.java
+++ BookingClone/src/main/java/com/xmlws/ws/controller/PoundaController.java
@@ -1,13 +1,11 @@
 package com.xmlws.ws.controller;
 
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -61,5 +59,13 @@ public class PoundaController {
 		}
 				
 	}
+		
+	@RequestMapping(value = "/ponuda/{id}" , method = RequestMethod.GET)
+	public ResponseEntity<Ponuda> getPonuda(@PathVariable Long id) {
+		
+		Ponuda ponuda = ponudaService.findOne(id);
+		
+		return new ResponseEntity<>(ponuda,HttpStatus.OK);
+	} 
 	
 }
